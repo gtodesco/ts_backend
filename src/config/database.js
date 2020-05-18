e63f22676538d57e220e727bbb47fb2d162e5cce
Gabriel Todesco <gabriel.todesco97@gmail.com>
2020-05-18 00:54:12 -0300

    db heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'root',
-    database: 'teamstats',
+    host: 'pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    username: 'i1ewi4tyjbqsip2f',
+    password: 'odl6v7wz3y4tpyop',
+    database: 'o4weaq6bx555lr9u',
     define: {
         timestamps: true,
         underscored: true,
@@ -11,3 +11,17 @@ module.exports = {
         updatedAt: 'updated_at',
     },
 };
+
+// module.exports = {
+//     dialect: 'mysql',
+//     host: 'localhost',
+//     username: 'root',
+//     password: 'root',
+//     database: 'teamstats',
+//     define: {
+//         timestamps: true,
+//         underscored: true,
+//         createdAt: 'created_at',
+//         updatedAt: 'updated_at',
+//     },
+// };
